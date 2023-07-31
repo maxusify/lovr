023c2156f99edf18ec0e2dd385e90ca4d1a0854d
Michał Pędzik <maxusify@gmail.com>
2023-07-31 15:27:22 +0200

    TailwindCSS, Http Client setup

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'ui';
+export class AppComponent implements OnInit {
+  public title: string = 'Lovr';
+  public users: any;
+
+  public constructor(private httpClient: HttpClient) {}
+
+  public ngOnInit(): void {
+    this.httpClient.get('https://localhost:5001/api/users').subscribe({
+      next: (response) => (this.users = response),
+      error: (error) => console.error(error),
+      complete: () => {},
+    });
+  }
 }

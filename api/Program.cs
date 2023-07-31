7fd808bd2c4fafe26ecbc5fc706e530918b97f27
Michał Pędzik <maxusify@gmail.com>
2023-07-31 14:07:54 +0200

    SQLite and Entity Framework

--- api/Program.cs
+++ api/Program.cs
@@ -1,5 +1,12 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 app.MapControllers();

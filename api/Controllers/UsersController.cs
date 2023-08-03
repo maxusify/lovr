8779a116cf213d53588bbf92068ffdb2ab6c2657
maxusify <michalpedzik.dev@gmail.com>
2023-08-03 15:02:11 +0200

    Controller inheritance

--- api/Controllers/UsersController.cs
+++ api/Controllers/UsersController.cs
@@ -5,27 +5,25 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class UsersController : ControllerBase
+public class UsersController : BaseController
 {
-  private readonly DataContext _context;
+    private readonly DataContext _context;
 
-  public UsersController(DataContext context)
-  {
-    _context = context;
-  }
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
 
-  [HttpGet]
-  public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-  {
-    var users = await _context.Users.ToListAsync();
-    return users;
-  }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+        return users;
+    }
 
-  [HttpGet("{id}")]
-  public async Task<ActionResult<AppUser>> GetUser(int id)
-  {
-    return await _context.Users.FindAsync(id);
-  }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
 }

6bf545966e30e1d7e9deb2ef338ce6804078d7d5
maxusify <michalpedzik.dev@gmail.com>
2023-08-03 12:57:37 +0200

    App pipeline explanation

--- api/Program.cs
+++ api/Program.cs
@@ -1,15 +1,35 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 
+// ===== CONFIGURATION =====
+
+// WebApplicationBuilder instance for configuring app
 var builder = WebApplication.CreateBuilder(args);
+
+// Add MVC service to the app
 builder.Services.AddControllers();
+
+// Add DataContext as a database context to the dependency injection container
 builder.Services.AddDbContext<DataContext>(opt =>
 {
-  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+// Add Cross-Origin Resource Sharing service
 builder.Services.AddCors();
 
+// ===== BUILDING =====
+
+// Build configured app
 var app = builder.Build();
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
+// Set up CORS middleware in the app pipeline
+app.UseCors(
+    builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200")
+);
+
+// Map the controllers to the app pipeline
 app.MapControllers();
+
+// Start the app and listen for requests
 app.Run();
